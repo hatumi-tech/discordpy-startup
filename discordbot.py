428fcc85fc099b96c6a32f4f788c5460c9a6dae4
hatumi-tech <73274049+hatumi-tech@users.noreply.github.com>
2020-10-23 00:54:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,14 +8,25 @@ import json
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-secretkey = os.environ['GOOGLE_SECRET_KEY']
 
 from oauth2client.service_account import ServiceAccountCredentials
+
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
 #認証情報設定
 #ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
-credentials = ServiceAccountCredentials.from_json_keyfile_name(secretkey, scope)
+credential = {
+                "type": "service_account",
+                "project_id": os.environ['SHEET_PROJECT_ID'],
+                "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+                "private_key": os.environ['SHEET_PRIVATE_KEY'],
+                "client_email": os.environ['SHEET_CLIENT_EMAIL'],
+                "client_id": os.environ['SHEET_CLIENT_ID'],
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
+             }
 
 #OAuth2の資格情報を使用してGoogle APIにログインします。
 gc = gspread.authorize(credentials)
@@ -70,5 +81,4 @@ async def gacha(ctx):
 async def ping(ctx):
     await ctx.send('pong')
 
-    
 bot.run(token)
