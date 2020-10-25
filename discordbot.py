c333e628bfcd91ecde81a2f91c35aa53785bf511
hatumi-tech <73274049+hatumi-tech@users.noreply.github.com>
2020-10-25 13:22:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -156,7 +156,7 @@ async def on_message(message):
          message_send = message_send + tokuseimessage
          message_send = message_send + "```"
         
-    await message.channel.send(message_send)
+         await message.channel.send(message_send)
          
     elif re.match('.+のすばやさ$', message.content):
       
@@ -181,6 +181,6 @@ async def on_message(message):
          message_send = message_send + text.format(m,speed1,saisoku1)
          message_send = message_send + "```"
 
-    await message.channel.send(message_send)
+         await message.channel.send(message_send)
             
 bot.run(token)
