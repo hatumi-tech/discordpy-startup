31e047e3eb54e20d6445bf245ebea62457b3113c
hatumi-tech <73274049+hatumi-tech@users.noreply.github.com>
2020-10-25 13:00:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -179,6 +179,6 @@ async def on_message(message):
          message_send = message_send + text.format(m,speed1,saisoku1)
          message_send = message_send + "```"
           
-     await message.channel.send(message_send)
+    await message.channel.send(message_send)
             
 bot.run(token)
