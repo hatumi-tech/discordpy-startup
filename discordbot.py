a3b9fe339f00def0e3795381c2914d432e919d81
hatumi-tech <73274049+hatumi-tech@users.noreply.github.com>
2020-10-28 04:26:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -564,6 +564,7 @@ async def on_message(message):
          
          await message.channel.send("調整先のポケモンを送信してください。")
          m2 = await client.wait_for('message', check=None)
+         m2 = str(m2)
          
          if m2 == "":
              await message.channel.send("何をしようとしているのですか。その行為に意味はありますか")
