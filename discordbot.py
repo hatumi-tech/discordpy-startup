0ae44f107320921019d7d29578556b3857324f19
hatumi-tech <73274049+hatumi-tech@users.noreply.github.com>
2021-05-01 21:48:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -79,7 +79,7 @@ async def on_message(message):
            return
         
         L1 = set( random.sample(range(1,1348), m) )
-         for umanumber in L1:
+        for umanumber in L1:
          uma = worksheet.cell(cell.row,umanumber).value
          print(uma)
             
