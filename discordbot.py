ae674be8e8c6036d8988bc840ff45ff5bdb6e1ec
hatumi-tech <73274049+hatumi-tech@users.noreply.github.com>
2021-06-12 20:45:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,7 @@ async def on_command_error(ctx, error):
 async def on_message(message):
     if message.author.bot:
         return
-    elif type(message.channel) == discord.DMChannel and client.user == message.channel.me:
+    elif type(message.channel) == discord.DMChannel and discord.ClientUser == message.channel.me:
         print(message.content)
     
 bot.run(token)
