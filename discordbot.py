8a127d3d670f40b45554cb58fe61117274a3a322
hatumi-tech <73274049+hatumi-tech@users.noreply.github.com>
2021-06-16 21:47:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -94,11 +94,11 @@ async def on_message(message):
         info = ("ちよは競馬ポーカーの準備を完了しました。")
         await message.channel.send(info)
 
-    elif message.content.endswith('頭の馬配って') and type(message.channel) == discord.DMChannel:
+    elif message.content.startswith('馬配って') and type(message.channel) == discord.DMChannel:
         DATABASE_INDEX = 2
 
         name = str(message.author)
-        horse_num = message.content[0:len(message.content)-6]
+        horse_num = message.content[0:len(message.content)-4]
 
         if horse_num > 10:
             info = ("ちよは10以上の数字がわかりません・・・。")
