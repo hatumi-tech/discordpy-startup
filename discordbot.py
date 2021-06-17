07540eda44e9807c0672465e9652bdb7f43406a4
hatumi-tech <73274049+hatumi-tech@users.noreply.github.com>
2021-06-17 19:01:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -68,6 +68,8 @@ async def on_message(message):
             return
 
         else:
+            infoall = ""
+            
             for i in range(horse_num):
                 info = r.spop("horse_name_all")
                 info = utf8(info)
